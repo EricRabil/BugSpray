458842915b3ca376193af76cb2385fe3a87c34a4
Eric Rabil <ericrabil@tregmine.com>
2016-12-20 15:38:24 -0500

    Just render home for now

--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,3 @@
 <?php
-$example = array ();
-$example ["title"] = "This is an example!";
-$context = array ();
-$context ["example"] = $example;
-$context ["settings"] = $settings;
-$styles = array ();
-$scripts = array ();
-render ( 'index.phtml', 'Backbones', $context, $styles, $scripts );
+render("index.phtml", "BugSpray", $context = array(), $styles = array(), $scripts = array(), "HS::Home");
+?>
