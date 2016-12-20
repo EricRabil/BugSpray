32549146254d5af0ff6bc26371ba7510310c3c5e
Eric Rabil <ericrabil@tregmine.com>
2016-12-20 15:37:18 -0500

    Removed all old code

--- pages/setup.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = array ();
-$data ["tabTitle"] = "Setup BugSpray";
-$context = array ();
-$context ["data"] = $data;
-$context ["settings"] = $settings;
-$styles = array ();
-$scripts = array ();
-render ( 'setup.phtml', 'Backbones', $context, $styles, $scripts );
