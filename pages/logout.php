16c010b48399195a4fb3ee8a2622207c3cf7416c
Eric Rabil <ericrabil@tregmine.com>
2016-12-27 17:45:33 +0100

    Invalidate remember-me after logout

--- pages/logout.php
+++ pages/logout.php
@@ -3,5 +3,6 @@ $_SESSION['user']['session_active'] = false;
 unset($_SESSION['user']['email']);
 unset($_SESSION['user']['displayname']);
 unset($_SESSION['user']['rank']);
+setcookie($config['security']['rememberMeCookieKey'], "invalidated-by-user-action", 1);
 header('Location: '.$config['general']['host'].'/');
 ?>
