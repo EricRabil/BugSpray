6387f6578c8c464dc91b5a51912a4a2c07703113
Eric Rabil <ericrabil@tregmine.com>
2016-12-27 18:01:41 +0100

    Revoke token after signout

--- pages/logout.php
+++ pages/logout.php
@@ -3,6 +3,10 @@ $_SESSION['user']['session_active'] = false;
 unset($_SESSION['user']['email']);
 unset($_SESSION['user']['displayname']);
 unset($_SESSION['user']['rank']);
+$rememberMeData = explode(':', $_COOKIE[$config['security']['rememberMeCookieKey']]);
+$sql = "DELETE FROM persistence_tokens WHERE selector = ?";
+$stmt = getConn()->prepare($sql);
+$stmt->execute(array($rememberMeData[0]));
 setcookie($config['security']['rememberMeCookieKey'], "invalidated-by-user-action", 1);
 header('Location: '.$config['general']['host'].'/');
 ?>
