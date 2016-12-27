f53d52c7beecdcdacb3ca1a7424c528b0b9ed502
Eric Rabil <ericrabil@tregmine.com>
2016-12-27 22:17:50 +0100

    Made login page more informative

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,5 @@
 <?php
 if(isset($_SESSION['pages']['login']['error'])){
-  echo $_SESSION['pages']['login']['error'];
+  echo $_SESSION['pages']['login']['error'].' - '.$lang['errors'][$_SESSION['pages']['login']['error']];
 }
 ?>
